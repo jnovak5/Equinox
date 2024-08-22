01919618df0a945fd0f20bb85d522186ce74c5f0
Jon Novak <jcnovak5@gmail.com>
2024-08-21 19:07:04 -0500

    First

---
+++ js/script.js
@@ -0,0 +1,166 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["", "Five Zombies Types!", "Monthly Events!", "Mod Suggestions!", ""],
+    typeSpeed:130,
+    BackSpeed:90,
+    loop:true
+})
+
+/* ============================== ES6 Class EasyHTTP ============================ */
+
+// Creating ES6 Class
+class EasyHTTP {
+
+    // Make an HTTP PUT Request
+    async put(url, data) {
+
+        // Awaiting fetch which contains method,
+        // headers and content-type and body
+        const response = await fetch(url, {
+             method: 'PUT',
+             headers: {
+                'Content-type': 'application/json'
+             },
+             body: JSON.stringify(data)
+        });
+
+        // Awaiting response.json()
+        let resData = await response.json();
+
+        // Return response data
+        return resData;
+    }
+}
+
+/* ============================== Aside ============================ */
+const nav = document.querySelector(".nav"),
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length,
+      allSection = document.querySelectorAll(".section"),
+      totalSection = allSection.length;
+      for(let i=0; i<totalNavList; i++)
+      {
+          const a = navList[i].querySelector("a");
+          a.addEventListener("click", function()
+          {
+              removeBackSection();
+              for(let j=0; j<totalNavList; j++)
+              {
+                  if(navList[j].querySelector("a").classList.contains("active"))
+                  {
+                      addBackSection(j);
+                     // allSection[j].classList.add("back-section");
+                  }
+                  navList[j].querySelector("a").classList.remove("active");
+              }
+              this.classList.add("active")
+              showSection(this);
+              if(window.innerWidth < 1200)
+              {
+                  asideSectionTogglerBtn();
+              }
+          })
+      }
+      function removeBackSection()
+      {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("back-section");
+        }   
+      }
+      function addBackSection(num)
+      {
+        allSection[num].classList.add("back-section");
+      }
+      function showSection(element)
+      {
+          for(let i=0; i<totalSection; i++)
+          {
+              allSection[i].classList.remove("active");
+          }
+          const target = element.getAttribute("href").split("#")[1];
+          document.querySelector("#" + target).classList.add("active")
+      }
+      function updateNav(element)
+      {
+          for(let i=0; i<totalNavList; i++)
+          {
+              navList[i].querySelector("a").classList.remove("active");
+              const target = element.getAttribute("href").split("#")[1];
+              if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+              {
+                navList[i].querySelector("a").classList.add("active");
+              }
+          }
+      }
+      document.querySelector(".hire-me").addEventListener("click", function()
+      {
+          const sectionIndex = this.getAttribute("data-section-index");
+          //console.log(sectionIndex);
+          showSection(this);
+          updateNav(this);
+          removeBackSection();
+          addBackSection(sectionIndex);
+      })
+      const navTogglerBtn = document.querySelector(".nav-toggler"),
+            aside = document.querySelector(".aside");
+            navTogglerBtn.addEventListener("click", () => 
+            {
+                asideSectionTogglerBtn();
+            })
+            function asideSectionTogglerBtn()
+            {
+                aside.classList.toggle("open");
+                navTogglerBtn.classList.toggle("open");
+                for(let i=0; i<totalSection; i++ )
+                {
+                    allSection[i].classList.toggle("open");
+                }
+            }
+/* ============================== Display Visiter Counter ============================ */
+
+// Fetching Count Method
+fetch("https://yabrjwjfq5.execute-api.us-east-1.amazonaws.com/test/counter/getCount")
+            // Convert data to JSON
+            .then(response => response.json())
+            .then(visitCount => {
+                
+                // Create var to store HTML
+                let visits = JSON.stringify(visitCount)
+
+                // Finding the number within the string
+                visits = visits.match(/\d+/)
+                let actualVisiters = Number(visits)
+
+                // Updating actual
+                actualVisiters = Number(visits) + 1;
+                // Converting back to String
+                actualVisiters = actualVisiters.toString();
+                console.log(actualVisiters)
+                
+                /* ============================== Updateing Visiter Counter ============================ */
+
+                // Instantiating new EasyHTTP class
+                let http = new EasyHTTP;
+
+                // User Data
+                let data = {
+                    test: actualVisiters
+                }
+
+                // Update Post
+                http.put('https://yabrjwjfq5.execute-api.us-east-1.amazonaws.com/test/counter/updateCount',
+                data)
+                
+                // Updating index.html
+                document.getElementById("visiters").innerHTML = ' Total Visits: ' + actualVisiters;
+
+            })
+
+        // Resolving promise for response data
+        .then(data => console.log(data))
+
+        
+
+
+
